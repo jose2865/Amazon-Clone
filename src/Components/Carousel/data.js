649ecd51031e3c06daf1fcc424d7fba3b3f1e58f
jose2865 <debhi92@yahoo.com>
2025-05-20 16:11:33 -0500

    update

--- src/Components/Carousel/data.js
+++ src/Components/Carousel/data.js
@@ -1,7 +1,7 @@
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.jpg';
-import image5 from './images/image5.jpg';
+import image1 from "./Images/image1.jpg";
+import image2 from "./Images/image2.jpg";
+import image3 from "./Images/image3.jpg";
+import image4 from "./Images/image4.jpg";
+import image5 from "./Images/image5.jpg";
 
 export const imageList = [image1, image2, image3, image4, image5];
